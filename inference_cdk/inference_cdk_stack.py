e2ca123a732eb72c29b3a0bd0fe2c311bb2f1401
Camilo Rozo <ce.rozob@uniandes.edu.co>
2025-04-05 23:55:08 -0500

    pytorch model pretrain done

--- inference_cdk/inference_cdk_stack.py
+++ inference_cdk/inference_cdk_stack.py
@@ -5,7 +5,8 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class SrcStack(Stack):
+
+class Inference_CDK_Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
